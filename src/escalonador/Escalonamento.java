3bbbde29ec4f65db17d4129d9077f4f51eaa9ed8
AbacherlyGabriel <gaabacherly@usp.br>
2018-10-15 14:51:59 -0300

    Parte de E/S pronta

--- src/escalonador/Escalonamento.java
+++ src/escalonador/Escalonamento.java
@@ -1,10 +1,6 @@
 package escalonador;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Escalonamento {
     
